664c36651ac7208585cc7858209856aaa667e22a
m.sabatowski <ma.sabatowski@gmail.com>
2022-04-11 16:53:04 +0200

    function loadDF added, test modified

--- src/main/scala/Mohamed.scala
+++ src/main/scala/Mohamed.scala
@@ -1,7 +1,9 @@
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.StringType
 
-object Mohamed {
+
+object Mohamed extends App {
 
   import org.apache.spark.sql.SparkSession
 
@@ -11,11 +13,23 @@ object Mohamed {
     .master("local[*]")
     .getOrCreate()
 
+  import spark.implicits._
+  val path = if (args.length > 0) args(0) else "cities.csv"
+  val df = loadDF(path)
 
-  def colToUpper(path: String, col: String) = {
-    val df: DataFrame = spark.read.options(Map("delimiter" -> ",", "header" -> "true", "inferSchema" -> "true")).csv(path)
-    val listCols = col.split("\\W+").filter(df.schema(_).dataType.typeName == "string")
-    df.select(df("*") +: listCols.map(col => upper(df(col)).as(s"upper_$col")) : _*)
+  def colToUpper(df: DataFrame, cols: Seq[String]) = {
+    val listCols = cols.filter(df.schema(_).dataType == StringType)
+    listCols.foldLeft(df) { (result, s) =>
+       result.withColumn(s"upper_$s", upper(df(s)))
+    }
+//    df.select(df("*") +: listCols.map(col => upper(df(col)).
+//      as(s"upper_$col")): _*)
   }
 
+  def loadDF(path: String): DataFrame = {
+    spark.read.options(
+      Map("delimiter" -> ",", "header" -> "true", "inferSchema" -> "true"))
+      .csv(path)
+  }
+  colToUpper(df, Seq("country", "city")).show
 }
--- src/test/scala/MohamedSpec.scala
+++ src/test/scala/MohamedSpec.scala
@@ -1,6 +1,6 @@
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
-import scala.collection.mutable._
+
 class MohamedSpec extends AnyFlatSpec with should.Matchers {
 
   "colToUpper" should "read a csv file, turn it into a data frame, " +
@@ -8,12 +8,19 @@ class MohamedSpec extends AnyFlatSpec with should.Matchers {
     "add upper case columns to the existing data frame named 'upper_colname'" in {
     import Mohamed._
 
-    colToUpper("cities.csv", "country,city").collect.map(_.toSeq) shouldBe ???
-//      Array(WrappedArray(0, "Warsaw", "Poland", "POLAND", "WARSAW"),
-//      WrappedArray(1, "Villeneuve-Loubet", "France", "FRANCE", "VILLENEUVE-LOUBET"),
-//      WrappedArray(2, "Vranje", "Serbia", "SERBIA", "VRANJE"),
-//      WrappedArray(3, "Pittsburgh", "US", "US", "PITTSBURGH"))
+    import spark.implicits._
 
+    val df = loadDF("cities.csv")
+    val cols = Seq("country", "city")
 
+    val actual = colToUpper(df, cols)
+    val expected = Seq(
+      (0, "Warsaw", "Poland", "POLAND", "WARSAW"),
+      (1, "Villeneuve-Loubet", "France", "FRANCE", "VILLENEUVE-LOUBET"),
+      (2, "Vranje", "Serbia", "SERBIA", "VRANJE"),
+      (3, "Pittsburgh", "US", "US", "PITTSBURGH")
+    ).toDF("id", "city", "country", "upper_city", "upper_country")
+
+    actual shouldBe expected
   }
 }
